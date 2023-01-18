9eb9bf7fc9b84141b1d76618d876642cc86a9881
Park Eun-Jeong <35605637+Dreamofheaven@users.noreply.github.com>
2023-01-18 15:15:29 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,7 @@
+S = input()
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+cnt = ''
+for i in alphabet:
+    cnt += str(S.find(i))
+    cnt += " " 
+print(cnt)
