7d7e44b7f48be7a472c718b870342f6d859b98ea
Park Eun-Jeong <35605637+Dreamofheaven@users.noreply.github.com>
2023-01-20 17:39:54 +0900

    [D3] Title: 소득 불균형, Time: 247 ms, Memory: 63,048 KB -BaekjoonHub

---
+++ SWEA/D3/10505. 소득 불균형/소득 불균형.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for t in range(T):
+    test_case = int(input())
+    income = list(map(int, input().split()))
+    average = sum(income) // test_case
+    cnt = 0
+
+    for i in income:
+        if i <= average:
+            cnt += 1
+    print(f'#{t+1} {cnt}')
