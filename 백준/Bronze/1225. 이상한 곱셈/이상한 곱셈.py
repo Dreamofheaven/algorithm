3d599630f46d1923897208cedb4b73f2bdd128ff
Park Eun-Jeong <35605637+Dreamofheaven@users.noreply.github.com>
2023-01-30 16:22:06 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 36 ms, Memory: 30620 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
@@ -1,9 +1,9 @@
 # 이상한 곱셈
-A, B = map(str, input().split())
-result = 0
-
-for i in A:
-    for j in B:
-        result += int(i)*int(j)
-print(result)
-    
+A, B = input().split()
+list_A = list(map(int, A))
+list_B = list(map(int, B))
+# for i in A:
+#     for j in B:
+#         result += int(i)*int(j)
+# print(result)
+print(sum(list_A) * sum(list_B))
